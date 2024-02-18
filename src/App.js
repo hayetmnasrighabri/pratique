da98b1c50d7d1dd92155e1d03caec8d0baf281b1
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-18 16:22:26 +0100

    les Portals

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import useFetch from "./useFetch"
+import { createPortal } from "react-dom"
+
 function App(){
-  const{data, loading, errors}=useFetch('https://jsonplaceholder.typicode.com/posts?_limit=18&_delay=2000')
-  return( <div>
-    {loading && <div>chargement...</div>}
-    {data && <div>
-      <ul>
-        {data.map(post=>(<li key={post.id}>{post.title}</li>))}
-      </ul>
-      </div>}
-  </div>
-)}
+  return(
+    <div style={{
+      height:300,
+      overflow:'scroll',
+      background:'#EEE',
+      margin: 20,
+      position:'relative'
+    }}>
+      <p>
+        Incididunt consequat mollit consequat ea nisi sint ex reprehenderit. Sunt velit id duis nulla laboris nostrud in. Nulla veniam excepteur ipsum esse incididunt sint. In nostrud labore ad quis ea ipsum incididunt veniam anim exercitation consectetur. Occaecat eiusmod commodo in ea adipisicing ipsum voluptate irure velit cupidatat. Est veniam reprehenderit occaecat dolore dolor anim aute incididunt exercitation ut. Exercitation dolore id dolor irure laborum fugiat deserunt mollit qui sit ea.
+
+        Enim sit deserunt duis officia aute ut non laborum tempor qui qui pariatur occaecat veniam. Sit amet voluptate minim duis qui id ad. Laborum ad deserunt et ex ullamco aliqua reprehenderit id consequat qui fugiat.
+
+        Eiusmod voluptate aliqua pariatur laborum sit. Nulla laborum est sit ea nulla ad fugiat. Magna culpa ad ex veniam nostrud aliqua laboris sit ullamco aliqua mollit. Sunt aute cillum est et veniam qui consectetur ea sint tempor culpa reprehenderit.
+      </p>
+      <p>
+        Incididunt consequat mollit consequat ea nisi sint ex reprehenderit. Sunt velit id duis nulla laboris nostrud in. Nulla veniam excepteur ipsum esse incididunt sint. In nostrud labore ad quis ea ipsum incididunt veniam anim exercitation consectetur. Occaecat eiusmod commodo in ea adipisicing ipsum voluptate irure velit cupidatat. Est veniam reprehenderit occaecat dolore dolor anim aute incididunt exercitation ut. Exercitation dolore id dolor irure laborum fugiat deserunt mollit qui sit ea.
+
+        Enim sit deserunt duis officia aute ut non laborum tempor qui qui pariatur occaecat veniam. Sit amet voluptate minim duis qui id ad. Laborum ad deserunt et ex ullamco aliqua reprehenderit id consequat qui fugiat.
+
+        Eiusmod voluptate aliqua pariatur laborum sit. Nulla laborum est sit ea nulla ad fugiat. Magna culpa ad ex veniam nostrud aliqua laboris sit ullamco aliqua mollit. Sunt aute cillum est et veniam qui consectetur ea sint tempor culpa reprehenderit.
+      </p>
+      <p>
+        Incididunt consequat mollit consequat ea nisi sint ex reprehenderit. Sunt velit id duis nulla laboris nostrud in. Nulla veniam excepteur ipsum esse incididunt sint. In nostrud labore ad quis ea ipsum incididunt veniam anim exercitation consectetur. Occaecat eiusmod commodo in ea adipisicing ipsum voluptate irure velit cupidatat. Est veniam reprehenderit occaecat dolore dolor anim aute incididunt exercitation ut. Exercitation dolore id dolor irure laborum fugiat deserunt mollit qui sit ea.
+
+        Enim sit deserunt duis officia aute ut non laborum tempor qui qui pariatur occaecat veniam. Sit amet voluptate minim duis qui id ad. Laborum ad deserunt et ex ullamco aliqua reprehenderit id consequat qui fugiat.
+
+        Eiusmod voluptate aliqua pariatur laborum sit. Nulla laborum est sit ea nulla ad fugiat. Magna culpa ad ex veniam nostrud aliqua laboris sit ullamco aliqua mollit. Sunt aute cillum est et veniam qui consectetur ea sint tempor culpa reprehenderit.
+      </p>
+       <Modal/>
+      </div>
+  )
+
+  function Modal(){
+    return(
+      createPortal(<div style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        padding:0,
+        border: 'solid 1px grey',
+        background: '#FFF'
+      }} >
+        je suis une Modal
+
+      </div>, document.body)
+    
+  )}
+}
 export default App
