dc181ded314ee40f998813d9ec6d04f52adf8bbc
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-20 20:05:40 +0100

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import React from 'react'
-import Products from './components/products'
-import { ErrorBoundary } from 'react-error-boundary'
-function App() {
-  return (
-    <div>
-      <ErrorBoundary>
-      <Products product="orange"/>
-      </ErrorBoundary>
-      <ErrorBoundary>
-      <Products product="banane"/>
-      </ErrorBoundary>
-      <ErrorBoundary>
-      <Products product="apple"/>
-      </ErrorBoundary>
-    </div>
+import { type } from "@testing-library/user-event/dist/type"
+import React, {useReducer} from "react"
+function Reducer(){
+  const initialState={
+    UserName: " ",
+    Email: " ",
+    Notes: " "
+  }
+  const reducer=(state, action)=>{
+    switch(action.type){
+      case 'input':
+        return {...state,[action.field]: action.value}
+      case "reset":
+        return initialState;
+       default:
+        return state;
+      }
+  }
+  const [state, dispatch]=useReducer(reducer, initialState)
+  const handleReset=()=>{
+    dispatch({type: "reset "})
+  }
+  const handleChange=(e)=>{
+    dispatch({
+      type: 'input',
+      field: e.target.name,
+      value: e.target.value
+    })
+    
+  }
+  const handleSubmit=(e)=>{
+   e.preventDefault()
+   console.log(state)
+   return state
+  }
+ 
+  return(
+    <form onSubmit={handleSubmit}>
+    <label>UserName
+      <input type="text" name="UserName" value={state.UserName} onChange={handleChange}/>
+    </label><br/><br/>
+    <label>Email
+      <input type="text" name="Email" value={state.Email} onChange={handleChange}/>
+    </label><br/><br/>
+    <label>Notes:
+      <input type="text" name="Notes" value={state.Notes} onChange={handleChange}/>
+    </label><br/><br/>
+    <button type="button" >Submit</button>
+    <button type="button" onClick={handleReset}>Reset</button>
+    </form>
   )
 }
-
-export default App
+export default Reducer
