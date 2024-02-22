ae6234b3d47c900d94053b17d115a6958ddddbc3
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-22 14:47:40 +0100

    Contexte : une alternative au passage d'accessoires

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Page from './context/Page'
-function App() {
+import Heading from "./heading";
+import Section from "./section";
+export default function Page() {
   return (
-   
-      <Page/>
-   
-
-  )
+    <Section>
+      <Heading level={1}>Title</Heading>
+      <Heading level={2}>Heading</Heading>
+      <Heading level={3}>Sub-heading</Heading>
+      <Heading level={4}>Sub-sub-heading</Heading>
+      <Heading level={5}>Sub-sub-sub-heading</Heading>
+      <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+    </Section>
+  );
 }
-
-export default App
