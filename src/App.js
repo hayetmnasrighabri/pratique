fc035d497c6a6ff0b50b313dec58aea7f06ccd9e
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-22 15:09:59 +0100

    créer le contexte

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ export default function Page() {
   return (
     <Section>
       <Heading level={1}>Title</Heading>
+  
+    <section>
       <Heading level={2}>Heading</Heading>
+      <Heading level={2}>Heading</Heading>
+      <Heading level={2}>Heading</Heading>
+      </section>
+    <Section>
       <Heading level={3}>Sub-heading</Heading>
+      <Heading level={3}>Sub-heading</Heading>
+      <Heading level={3}>Sub-heading</Heading>
+  <Section>
       <Heading level={4}>Sub-sub-heading</Heading>
-      <Heading level={5}>Sub-sub-sub-heading</Heading>
-      <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+    </Section>
+    </Section>
     </Section>
   );
 }
